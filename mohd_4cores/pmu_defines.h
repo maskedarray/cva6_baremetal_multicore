c12aeb51dfbfdc19a6317272709231d37ba1923d
maskedarray <rao.arrn@gmail.com>
2023-11-15 17:42:21 -0500

    added pmu_defines

--- mohd_4cores/pmu_defines.h
+++ mohd_4cores/pmu_defines.h
@@ -27,10 +27,9 @@
 ///     | Counter                            |
 ///     \************************************/
 ///     /************************************\
-///     | (PMU Core) Status Register         |          Not yet 4kB aligned
-///     | MemGuard Period Register           |                  PMU Bundle
-///     | PMU Timer                          |              
-///     \************************************/		PMU_BASE_ADDRESS
+///     | MemGuard Period Register           |          Not yet 4kB aligned
+///     | PMU Timer                          |              PMU Bundle
+///     \************************************/
 /// Each block is a separate 4kB-aligned page.
 /// The PMU Bundle includes the PMU Timer and the MemGuard Period Register.
 /// A counter bundle includes:
@@ -39,22 +38,24 @@
 ///     3. Event Info Configuration Register      BASE_ADDR + 0x8
 ///     4. Initial Budget Register                BASE_ADDR + 0xc
 
-#define NUM_COUNTER     8
+#define NUM_COUNTER 8
 
 #define TIMER_WIDTH     0x8
 #define STATUS_WIDTH    0x4
+#define BOOT_ADDR_WIDTH 0x4
 #define COUNTER_WIDTH   0x4
 
 // PMU Bundle Addresses
 #define PMU_B_BASE_ADDR     0x10404000
 #define TIMER_ADDR          (PMU_B_BASE_ADDR)
 #define PERIOD_ADDR         (PMU_B_BASE_ADDR + TIMER_WIDTH)
-#define STATUS_ADDR         (PMU_B_BASE_ADDR + 2*TIMER_WIDTH)
+#define PMC_STATUS_ADDR     (PMU_B_BASE_ADDR + 2*TIMER_WIDTH)
+#define PMC_BOOT_ADDR       (PMU_B_BASE_ADDR + 2*TIMER_WIDTH + STATUS_WIDTH)
 // Two 64-bit (8B) timer and one 32-bit status registers in the PMU bundle.
-#define PMU_BUNDLE_SIZE     2*TIMER_WIDTH + STATUS_WIDTH
+#define PMU_BUNDLE_SIZE     2*TIMER_WIDTH + STATUS_WIDTH + BOOT_ADDR_WIDTH
 
 // Counter Bundle Base Addresses
-#define COUNTER_B_BASE_ADDR     (PMU_B_BASE_ADDR     + PMU_BUNDLE_SIZE)
+#define COUNTER_B_BASE_ADDR     (PMU_B_BASE_ADDR + PMU_BUNDLE_SIZE)
 #define COUNTER_BASE_ADDR       (COUNTER_B_BASE_ADDR + 0*COUNTER_WIDTH)
 #define EVENT_SEL_BASE_ADDR     (COUNTER_B_BASE_ADDR + 1*COUNTER_WIDTH)
 #define EVENT_INFO_BASE_ADDR    (COUNTER_B_BASE_ADDR + 2*COUNTER_WIDTH)
@@ -63,5 +64,67 @@
 #define COUNTER_BUNDLE_SIZE     16
 
 // PMU Core Addresses
-#define ISPM_BASE_ADDR      0x10405000
-#define DSPM_BASE_ADDR      0x10406000
+#define ISPM_BASE_ADDR  0x10405000
+#define DSPM_BASE_ADDR  0x10406000
+
+/// **********************************************************************
+/// PMU Event Defines for Event Selection Register
+/// **********************************************************************
+/// Defines for Core to/from LLC
+/// ****************************
+// Read requests from Core X to LLC
+#define RD_REQ_CORE_0   0x2F001F
+#define RD_REQ_CORE_1   0x3F001F
+#define RD_REQ_CORE_2   0x4F001F
+#define RD_REQ_CORE_3   0x5F001F
+// Read responses to Core X from LLC
+#define RD_RES_CORE_0   0x2F003F
+#define RD_RES_CORE_1   0x3F003F
+#define RD_RES_CORE_2   0x4F003F
+#define RD_RES_CORE_3   0x5F003F
+// Write requests from Core X to LLC
+#define WR_REQ_CORE_0   0x2F002F
+#define WR_REQ_CORE_1   0x3F002F
+#define WR_REQ_CORE_2   0x4F002F
+#define WR_REQ_CORE_3   0x5F002F
+// Write responses to Core X from LLC
+#define WR_RES_CORE_0   0x2F004F
+#define WR_RES_CORE_1   0x3F004F
+#define WR_RES_CORE_2   0x4F004F
+#define WR_RES_CORE_3   0x5F004F
+
+/// ***********************************
+/// Defines for LLC to/from Main Memory
+/// ***********************************
+// Read and write requests of all cores to Main Memory from LLC
+#define RD_REQ_MEMORY   0x1F001F  
+#define WR_REQ_MEMORY   0x1F002F
+// Read and write responses of all cores to LLC from Main Memory
+#define RD_RES_MEMORY   0x1F003F  
+#define WR_RES_MEMORY   0x1F004F
+
+// Read and write requests of Core X to Main Memory from LLC
+#define RD_REQ_MEMORY_FROM_CORE_0  0x1F4F1F
+#define RD_REQ_MEMORY_FROM_CORE_1  0x1F5F1F
+#define RD_REQ_MEMORY_FROM_CORE_2  0x1F6F1F
+#define RD_REQ_MEMORY_FROM_CORE_3  0x1F7F1F
+#define WR_REQ_MEMORY_FROM_CORE_0  0x1F4F2F
+#define WR_REQ_MEMORY_FROM_CORE_1  0x1F5F2F
+#define WR_REQ_MEMORY_FROM_CORE_2  0x1F6F2F
+#define WR_REQ_MEMORY_FROM_CORE_3  0x1F7F2F
+// Read and write responses of Core X to LLC from Main Memory
+#define RD_RES_MEMORY_FROM_CORE_0  0x1F4F3F
+#define RD_RES_MEMORY_FROM_CORE_1  0x1F5F3F
+#define RD_RES_MEMORY_FROM_CORE_2  0x1F6F3F
+#define RD_RES_MEMORY_FROM_CORE_3  0x1F7F3F
+#define WR_RES_MEMORY_FROM_CORE_0  0x1F4F4F
+#define WR_RES_MEMORY_FROM_CORE_1  0x1F5F4F
+#define WR_RES_MEMORY_FROM_CORE_2  0x1F6F4F
+#define WR_RES_MEMORY_FROM_CORE_3  0x1F7F4F
+
+/// **********************************************************************
+/// Defines for Event Info Register
+/// **********************************************************************
+/// Note: The following define only works if the response (X_RES_X) events are selected
+//        using the corresponding Event Select Register.
+#define ADD_RESP_LAT    0x8001E0
